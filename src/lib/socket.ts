b21a826cf26d4e804c789134c4596f942fd67da9
Cris Elias <cristian.ricardo.elias@gmail.com>
2023-04-16 15:22:01 -0300

    Remove logs

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -3,10 +3,3 @@ import { io } from "socket.io-client";
 export const socket = io(
   import.meta.env.VITE_WEBSOCKET_SERVER_URL || "http://localhost:9001"
 );
-
-console.log(
-  "VITE_WEBSOCKET_SERVER_URL",
-  import.meta.env.VITE_WEBSOCKET_SERVER_URL
-);
-
-console.log("VITE_WEBSOCKET_SERVER_URL", import.meta.env.WEBSOCKET_SERVER_URL);
