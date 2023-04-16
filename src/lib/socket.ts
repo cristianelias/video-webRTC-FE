a65f92ed2063aef5ee9bd2b56260734dd6fa3f5c
Cris Elias <cristian.ricardo.elias@gmail.com>
2023-04-16 15:17:37 -0300

    Configs needed for deployment

--- src/lib/socket.ts
+++ src/lib/socket.ts
@@ -1,4 +1,12 @@
-const webSocketServerURL = import.meta.env.WEBSOCKET_SERVER_URL;
 import { io } from "socket.io-client";
 
-export const socket = io(webSocketServerURL || "http://localhost:9001");
+export const socket = io(
+  import.meta.env.VITE_WEBSOCKET_SERVER_URL || "http://localhost:9001"
+);
+
+console.log(
+  "VITE_WEBSOCKET_SERVER_URL",
+  import.meta.env.VITE_WEBSOCKET_SERVER_URL
+);
+
+console.log("VITE_WEBSOCKET_SERVER_URL", import.meta.env.WEBSOCKET_SERVER_URL);
